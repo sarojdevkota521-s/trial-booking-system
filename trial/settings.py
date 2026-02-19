c85959d8defc0ad5860d307f760b144cf5d0be83
saroj <sarojdevkota521@gmail.com>
2026-02-19 12:14:06 +0545

    updated

--- trial/settings.py
+++ trial/settings.py
@@ -128,9 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
